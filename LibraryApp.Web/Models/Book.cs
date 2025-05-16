323ed75e3289147a78106cacfbb7ae71e9633e14
1256ID <112385041+1256ID@users.noreply.github.com>
2025-05-16 10:25:10 +0200

    Adding Models/Services.

---
+++ LibraryApp.Web/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryApp.Web.Models;
+
+public class Book
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+}
---
+++ LibraryApp.Web/Services/BookService.cs
@@ -0,0 +1,13 @@
+﻿using LibraryApp.Web.Models;
+
+namespace LibraryApp.Web.Services;
+
+public class BookService
+{
+    private List<Book> books = [];
+
+    public void Add(Book book) => books.Add(book);
+    public Book? GetById(Guid id) => books.SingleOrDefault(c => c.Equals(id));
+    public Book[] GetAll() => books.ToArray();
+    public void Remove(Book book) => books.Remove(book);
+}
